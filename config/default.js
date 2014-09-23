dfe5deb379098b24441ba8d85041bc27682061b5
Michael Novotny <manovotny@gmail.com>
2014-09-23 10:49:41 -0500

    Bumps version for 1.0.1 release.

--- config/default.js
+++ config/default.js
@@ -25,7 +25,7 @@ module.exports = (function () {
             slug: 'wp-url-util',
             type: 'plugin', // Should be `plugin` or `theme`.
             url: 'https://github.com/manovotny/wp-url-util',
-            version: '1.0.0'
+            version: '1.0.1'
         }
     };
 
